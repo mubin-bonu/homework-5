f34300006b55b33c64082c3f48859d3d637282c6
mubin-bonu <mubin.me@mail.ru>
2023-04-01 13:50:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyExampleComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextState.count !== this.state.count;
+    }
+    componentDidUpdate(prevProps, prevState) {
+        console.log(`Count updated from ${prevState.count} to ${this.state.count}`);
+    }
+    handleClick = () => {
+        this.setState(prevState => ({ count: prevState.count + 1 }));
+    }
+    render() {
+        const { count } = this.state;
+        return (
+            <div>
+                <p>Считать: {count}</p>
+                <button onClick={this.handleClick}>Нажать</button>
+            </div>
+        );
+    }
 }
+export default MyExampleComponent;
 
-export default App;
